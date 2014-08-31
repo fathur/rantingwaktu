2b15549c0d94d1900539e1ed3e17c3b77152ab39
fathur <fathur_rohman17@yahoo.co.id>
2014-08-31 11:11:53 +0700

    update ignorefile and update vagrant bootstrap

--- VagrantBootstrap.sh
+++ VagrantBootstrap.sh
@@ -15,11 +15,14 @@ sudo apt-get install -y vim curl python-software-properties
 echo "--- We want the bleeding edge of PHP, right master? ---"
 sudo add-apt-repository -y ppa:ondrej/php5
 
+echo "--- Node JS repository ---"
+sudo add-apt-repository -y ppa:chris-lea/node.js
+
 echo "--- Updating packages list ---"
 sudo apt-get update
 
 echo "--- Installing PHP-specific packages ---"
-sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt mysql-server-5.5 php5-mysql git-core
+sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt mysql-server-5.5 php5-mysql git-core nodejs
 
 echo "--- Installing and configuring Xdebug ---"
 sudo apt-get install -y php5-xdebug
